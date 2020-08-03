b91ebfef56a1a4eccf3ec26879e736a6a5373120
Aamir Shehzad <shakeel.arshad30@gmail.com>
2020-08-04 01:15:11 +0500

    Update sendEmail.groovy

--- vars/sendEmail.groovy
+++ vars/sendEmail.groovy
@@ -28,7 +28,7 @@ def call(String recipients, String body) {
     emailext(subject: "Build $status - ${JOB_NAME} #${BUILD_NUMBER} ",
             body: """ 
                  job:  ${env.JOB_NAME} \n
-                 brnach: ${BRANCH_NAME} \n
+                 brnach: ${env.BRANCH_NAME} \n
             """
             , from: '"Jenkins server" <foo@acme.org>'
             , to: "$recipients")
