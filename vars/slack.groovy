8c3e0e44f3f958c4a4e4acfe489a958943378ede
Aamir Shehzad <shakeel.arshad30@gmail.com>
2020-08-07 23:34:37 +0500

    Update slack.groovy

--- vars/slack.groovy
+++ vars/slack.groovy
@@ -7,19 +7,19 @@ def call() {
   BUILD_URL="${BUILD_URL}/console"
     if(!env.BUILD_NUMBER.equals("1")){
         if ( buildResult == "SUCCESS") {
-          slackSend color: "good", USER: ${currentUser} ,message: "${env.JOB_NAME} - #${env.BUILD_NUMBER} Success after ${duration} (<${BUILD_URL}|Open>)"
+          slackSend color: "good", message: "${currentUser} - ${env.JOB_NAME} - #${env.BUILD_NUMBER} Success after ${duration} (<${BUILD_URL}|Open>)"
         }
         else if (buildResult == "FAILURE") {
-            slackSend color: "danger", USER: ${currentUser} , message: "${env.JOB_NAME} - #${env.BUILD_NUMBER} Failure after ${duration} (<${BUILD_URL}|Open>)"
+            slackSend color: "danger", message: "${currentUser} - ${env.JOB_NAME} - #${env.BUILD_NUMBER} Failure after ${duration} (<${BUILD_URL}|Open>)"
         }
         else if (buildResult == "STABLE") {
-            slackSend color: "good", USER: ${currentUser} ,message: "${env.JOB_NAME} - #${env.BUILD_NUMBER} Back to normal after ${duration} (<${BUILD_URL}|Open>)"
+            slackSend color: "good", message: "${currentUser} - ${env.JOB_NAME} - #${env.BUILD_NUMBER} Back to normal after ${duration} (<${BUILD_URL}|Open>)"
         }
         else if (buildResult == "UNSTABLE") {
-            slackSend color: "warning", USER: ${currentUser} ,message: "${env.JOB_NAME} - #${env.BUILD_NUMBER} Unstable after ${duration} (<${BUILD_URL}|Open>)"
+            slackSend color: "warning", message: "${currentUser} - ${env.JOB_NAME} - #${env.BUILD_NUMBER} Unstable after ${duration} (<${BUILD_URL}|Open>)"
         }
         else {
-            slackSend color: "danger", USER: ${currentUser} ,message: "${env.JOB_NAME} - #${env.BUILD_NUMBER} Unhandled result ${buildResult} after ${duration} (<${BUILD_URL}|Open>)"
+            slackSend color: "danger", message: "${currentUser}  - ${env.JOB_NAME} - #${env.BUILD_NUMBER} Unhandled result ${buildResult} after ${duration} (<${BUILD_URL}|Open>)"
         }
     }
 }
