0e51c10200c0aef17382fd1962788bff4ba07c40
Aamir Shehzad <shakeel.arshad30@gmail.com>
2020-08-11 01:08:29 +0500

    Update slack.groovy

--- vars/slack.groovy
+++ vars/slack.groovy
@@ -16,19 +16,19 @@ def call(String build_number) {
   def BUILD_URL="${BUILD_URL}console"
     if(!env.BUILD_NUMBER.equals("1")){
         if ( buildResult == "SUCCESS") {
-          slackSend color: "good", message: "Started Job: ${env.JOB_NAME} \n Details: ${env.BRANCH_NAME} #${env.BUILD_NUMBER} \n Status: Success after ${duration} \n Started By: ${currentUser} \n (<${BUILD_URL}|Open>)"
+          slackSend color: "good", message: "Started Job: ${env.JOB_NAME} \n Details: ${env.BRANCH_NAME} #${build_number} \n Status: Success after ${duration} \n Started By: ${currentUser} \n (<${BUILD_URL}|Open>)"
         }
         else if (buildResult == "FAILURE") {
-            slackSend color: "danger", message: "Started Job: ${env.JOB_NAME} \n Details: ${env.BRANCH_NAME} #${env.BUILD_NUMBER} \n Status: Failure after ${duration} \n Started By: ${currentUser} \n (<${BUILD_URL}|Open>)"
+            slackSend color: "danger", message: "Started Job: ${env.JOB_NAME} \n Details: ${env.BRANCH_NAME} #${build_number} \n Status: Failure after ${duration} \n Started By: ${currentUser} \n (<${BUILD_URL}|Open>)"
         }
         else if (buildResult == "STABLE") {
-            slackSend color: "good", message: "Started Job: ${env.JOB_NAME} \n Details: ${env.BRANCH_NAME} #${env.BUILD_NUMBER} \n Status: Back to normal after ${duration} \n Started By: ${currentUser} \n (<${BUILD_URL}|Open>)"
+            slackSend color: "good", message: "Started Job: ${env.JOB_NAME} \n Details: ${env.BRANCH_NAME} #${build_number} \n Status: Back to normal after ${duration} \n Started By: ${currentUser} \n (<${BUILD_URL}|Open>)"
         }
         else if (buildResult == "UNSTABLE") {
-            slackSend color: "warning", message: "Started Job: ${env.JOB_NAME} \n Details: ${env.BRANCH_NAME} #${env.BUILD_NUMBER} \n Status: Unstable after ${duration} \n Started By: ${currentUser} \n (<${BUILD_URL}|Open>)"
+            slackSend color: "warning", message: "Started Job: ${env.JOB_NAME} \n Details: ${env.BRANCH_NAME} #${build_number} \n Status: Unstable after ${duration} \n Started By: ${currentUser} \n (<${BUILD_URL}|Open>)"
         }
         else {
-            slackSend color: "danger", message: "Started Job: ${env.JOB_NAME} \n Details: ${env.BRANCH_NAME} #${env.BUILD_NUMBER} \n Status: Unhandled result ${buildResult} after ${duration} ${duration} \n Started By: ${currentUser} \n (<${BUILD_URL}|Open>)"
+            slackSend color: "danger", message: "Started Job: ${env.JOB_NAME} \n Details: ${env.BRANCH_NAME} #${build_number} \n Status: Unhandled result ${buildResult} after ${duration} ${duration} \n Started By: ${currentUser} \n (<${BUILD_URL}|Open>)"
         }
     }
 }
