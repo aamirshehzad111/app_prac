f9a8aef1e9c7c002cdb7e5527ab4f94072b8bff5
Aamir Shehzad <shakeel.arshad30@gmail.com>
2020-08-12 21:07:28 +0500

    Update slack.groovy

--- vars/slack.groovy
+++ vars/slack.groovy
@@ -2,33 +2,43 @@
 
 import hudson.model.*
 
-def getUser(int build_number){
-    def job = Jenkins.getInstance().getItemByFullName(env.JOB_BASE_NAME, Job.class)
-    def build =  job.getBuildByNumber(build_number)
-    def userId = build.getCause(Cause.UserIdCause).getUserId()
-    return userId
+def getUser(){
+    def job = Jenkins.instance.getItemByFullName(env.JOB_NAME, Job.class)
+    println job
+    def build =  job.getBuildByNumber(env.BUILD_NUMBER as int)
+    println build
+    try{
+        def userId = build.getCause().getUserId()
+        println "User id to notify to: " + userId
+        return userId
+    }catch(Exception ex) {
+        return "github-iniciated"
+    }
 }
 
-def call(String build_number) {
+def call() {
+  println "Getting user information"
   def buildResult = currentBuild.currentResult
-  def currentUser = getUser(build_number.toInteger())
+  def currentUser = getUser()
+  println "User id found"
   def duration = currentBuild.durationString.replace(' and counting', '')
   def BUILD_URL="${BUILD_URL}console"
     if(!env.BUILD_NUMBER.equals("1")){
+        println "Send slack notification"
         if ( buildResult == "SUCCESS") {
-          slackSend color: "good", message: "Started Job: ${env.JOB_NAME} \n Details: ${env.BRANCH_NAME} #${build_number} \n Status: Success after ${duration} \n Started By: ${currentUser} \n (<${BUILD_URL}|Open>)"
+            slackSend color: "good", message: "Started Job: ${env.JOB_NAME} \n Details: ${env.BRANCH_NAME} #${build_number} \n Status: Success after ${duration} \n Started By: ${currentUser} \n (<${BUILD_URL}|Open>)"
         }
         else if (buildResult == "FAILURE") {
-            slackSend color: "danger", message: "Started Job: ${env.JOB_NAME} \n Details: ${env.BRANCH_NAME} #${build_number} \n Status: Failure after ${duration} \n Started By: ${currentUser} \n (<${BUILD_URL}|Open>)"
+            slackSend color: "danger", message: "Started Job: ${env.JOB_NAME} \n Details: ${env.BRANCH_NAME} #${env.BUILD_NUMBER} \n Status: Failure after ${duration} \n Started By: ${currentUser} \n (<${BUILD_URL}|Open>)"
         }
         else if (buildResult == "STABLE") {
-            slackSend color: "good", message: "Started Job: ${env.JOB_NAME} \n Details: ${env.BRANCH_NAME} #${build_number} \n Status: Back to normal after ${duration} \n Started By: ${currentUser} \n (<${BUILD_URL}|Open>)"
+            slackSend color: "good", message: "Started Job: ${env.JOB_NAME} \n Details: ${env.BRANCH_NAME} #${env.BUILD_NUMBER} \n Status: Back to normal after ${duration} \n Started By: ${currentUser} \n (<${BUILD_URL}|Open>)"
         }
         else if (buildResult == "UNSTABLE") {
-            slackSend color: "warning", message: "Started Job: ${env.JOB_NAME} \n Details: ${env.BRANCH_NAME} #${build_number} \n Status: Unstable after ${duration} \n Started By: ${currentUser} \n (<${BUILD_URL}|Open>)"
+            slackSend color: "warning", message: "Started Job: ${env.JOB_NAME} \n Details: ${env.BRANCH_NAME} #${env.BUILD_NUMBER} \n Status: Unstable after ${duration} \n Started By: ${currentUser} \n (<${BUILD_URL}|Open>)"
         }
         else {
-            slackSend color: "danger", message: "Started Job: ${env.JOB_NAME} \n Details: ${env.BRANCH_NAME} #${build_number} \n Status: Unhandled result ${buildResult} after ${duration} ${duration} \n Started By: ${currentUser} \n (<${BUILD_URL}|Open>)"
+            slackSend color: "danger", message: "Started Job: ${env.JOB_NAME} \n Details: ${env.BRANCH_NAME} #${env.BUILD_NUMBER} \n Status: Unhandled result ${buildResult} after ${duration} ${duration} \n Started By: ${currentUser} \n (<${BUILD_URL}|Open>)"
         }
     }
 }
